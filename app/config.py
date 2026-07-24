a763ac6fabf4286989dd0fa270c72fd85f4a7a60
Sanyog-Faujdar <sanyogfaujdar@gmail.com>
2026-07-24 17:58:16 +0530

    Fix Railway DATABASE_URL configuration

--- app/config.py
+++ app/config.py
@@ -4,21 +4,23 @@ import os
 
 load_dotenv()
 
-class Config():
-    
+class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-    
-    USER = os.getenv("DB_USER")
-    PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
-    HOST = os.getenv("DB_HOST")
-    PORT = os.getenv("DB_PORT")
-    NAME = os.getenv("DB_NAME")
+
     DATABASE_URL = os.getenv("DATABASE_URL")
+
     if DATABASE_URL:
         SQLALCHEMY_DATABASE_URI = DATABASE_URL
     else:
+        USER = os.getenv("DB_USER")
+        PASSWORD = quote_plus(os.getenv("DB_PASSWORD"))
+        HOST = os.getenv("DB_HOST")
+        PORT = os.getenv("DB_PORT")
+        NAME = os.getenv("DB_NAME")
+
         SQLALCHEMY_DATABASE_URI = (
             f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{NAME}"
         )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
